b934403d6f64730733623900ee5cf0fc026f13b7
pras <qwe@rty.com>
2019-02-05 09:54:18 +0530

    rcom stock proce every 5 sec

--- test.js
+++ test.js
@@ -1,28 +1,64 @@
-var mongoose = require('mongoose')
+const axios = require('axios')
+var MongoClient = require('mongodb').MongoClient;
+const moment = require('moment')
 
-mongoose.connect("mongodb://localhost:27017/mydb");
+// var mongoose = require('mongoose')
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+// mongoose.connect("mongodb://localhost:27017/mydb");
 
-
-var kittySchema = mongoose.Schema({
-  name: String,
-  qwe: String
-});
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
 
 
-var Kitten1 = mongoose.model('Kitten', kittySchema);
+// var kittySchema = mongoose.Schema({
+//   name: String,
+//   qwe: String
+// });
+
+
+// var Kitten1 = mongoose.model('Kitten', kittySchema);
 
 
 
-db.once('open', function () {
-  console.log('db connection established')
-  var silence = new Kitten1({ name: 'Silence', });
-  console.log(silence);
-  silence.save()
+// db.once('open', function () {
+//   console.log('db connection established')
+//   var silence = new Kitten1({ name: 'Silence', });
+//   console.log(silence);
+//   silence.save()
 
-  Kitten1.find(function (err, docs) {
-    console.log(docs)
-  })
-});
+//   Kitten1.find(function (err, docs) {
+//     console.log(docs)
+//   })
+// });
+
+
+let rCom_COmpiID = 'RCV02'
+var url = "mongodb://localhost:27017/"
+
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    // res.status(500).send(err.message)
+    return
+  };
+  const mydb = db.db('mydb')
+  const clx = mydb.collection('rcom_seconds_log')
+  const todayDate = moment().format('YYYY-MM-DD')
+  //#endregion})
+  let interval = setInterval(() => {
+    if (moment().format('hhmm') < '1530') {
+      axios.get(`https://priceapi-aws.moneycontrol.com/pricefeed/nse/equitycash/${rCom_COmpiID}`).then(resp => {
+        let data = resp.data.data
+        // console.log(data.pricecurrent, data.VOL)
+
+        let dateTime = todayDate + ' ' + data[0]
+        clx.updateOne({ _id: dateTime }, { $set: { data } }, { upsert: true }).then(result => {
+          console.log('inserted', dateTime)
+        })
+
+      })
+    } else {
+      clearInterval(interval)
+    }
+
+  }, 5000)
+})
