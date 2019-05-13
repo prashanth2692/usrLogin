02738052d54b40b66b78961bc8cdb42b6423779a
Boddu Prashanth Kumar <prashanth2692@gmail.com>
2019-05-13 23:47:49 +0530

    logsFactory.js included UTC date.

--- helpers/logsFactory.js
+++ helpers/logsFactory.js
@@ -1,4 +1,6 @@
+//@ts-check
 const fallbackJobID = require('uuid/v1')()
+const moment = require("moment")
 
 function logsFactory(jobName, jobId) {
     this.log = function (type, message, moreInfo) {
@@ -7,6 +9,7 @@ function logsFactory(jobName, jobId) {
         this.type = type
         this.message = message
         this.moreInfo = moreInfo
+        this.date = moment.utc().format("YYYY/MM/DD HH:mm ss Z") // eg: 2019/05/13 18:15 40 +00:00
     }
 }
 
