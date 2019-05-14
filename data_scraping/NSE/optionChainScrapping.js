e192797b699edd586eff7df47fd9c599029b62cc
Boddu Prashanth Kumar <prashanth2692@gmail.com>
2019-05-14 17:43:40 +0530

    Option chain scraping initial commit.

---
+++ data_scraping/NSE/optionChainScrapping.js
@@ -0,0 +1,8 @@
+/**
+ * the link to option chain data for a stock and date is received in location
+ * header of response for below URL id date not provided else in the response
+ * check for response status
+ * if 302: check Location header
+ * if 200: check response body
+ * https://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?segmentLink=17&instrument=OPTSTK&symbol=ITC&date=30MAY2019
+ */
